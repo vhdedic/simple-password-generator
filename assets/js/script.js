fc0cb796dcaa518d5955fd0ea24c290195816b61
vhdedic <vdedic@gmail.com>
2021-09-01 10:50:49 +0200

    Edit assets/js/script.js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let upperCasesArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let lowerCasesArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let numbersArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let specialCharsArr = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"];
+
+const lowerCasesDOM = document.querySelector('#lowerCases');
+const upperCasesDOM = document.querySelector('#upperCases');
+const numbersDOM = document.querySelector('#numbers');
+const specialCharsDOM = document.querySelector('#specialChars');
+const passLengthDOM = document.querySelector('#passLength');
+const passNumberDOM = document.querySelector('#passNumber');
